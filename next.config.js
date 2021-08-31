cda3bb45f3798b88a9f7ea51290bedb5dc8cac97
Emmanuel Cortes <manetoso98@hotmail.com>
2021-08-31 09:11:44 -0500

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,2 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')(['bella-olonge-components'])
+module.exports = withTM()
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'bella-olonge-components/components/tokens/main.css'
+import 'bella-olonge-components/components/tokens/tokens.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
